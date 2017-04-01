8e0fff74f3b38cf05bfc68e1542cf068f80b4c0d
Arvind Iyer <arvindi.me@gmail.com>
2017-04-02 01:25:53 +0200

    Added Scripts and json library

---
+++ Scripts/GetHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+[Serializable]
+public class M2XSchema : MonoBehaviour
+{
+    public float Value { get; set; }
+
+    public static M2XSchema CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<M2XSchema>(jsonString);
+    }
+}
+
+class GetHelper : MonoBehaviour
+{
+    class ABC
+    {
+        public string Name;
+
+    }
+    void Start() { }
+    
+    public WWW GET(string url)
+    {
+        
+        WWW www = new WWW(url);
+        StartCoroutine(WaitForRequest(www));
+        return www;
+    }
+    public M2XSchema m2x()
+    {
+        string url = "http://api-m2x.att.com/v2/devices/a24ae3830e6418252dba44f657265be8/streams/outfingers";
+        WWW www = new WWW(url);
+        
+        StartCoroutine(WaitForRequest(www));
+        while (!www.isDone) ;
+        Debug.Log(www.text);
+        Debug.Log("WWWTEXT :" + www.text);
+
+        float value = float.Parse(new JSONObject(www.text)["value"].ToString());
+         
+        return new M2XSchema() {Value = value};
+            
+    }
+    public WWW POST(string url, Dictionary<string, string> post)
+    {
+        WWWForm form = new WWWForm();
+        foreach (KeyValuePair<String, String> post_arg in post)
+        {
+            form.AddField(post_arg.Key, post_arg.Value);
+        }
+        WWW www = new WWW(url, form);
+        StartCoroutine(WaitForRequest(www));
+        return www;
+    }
+    private IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.text);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+        Debug.Log("End req");
+    }
+}                                                  
---
+++ Scripts/MainController.cs
@@ -0,0 +1,93 @@
+﻿// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class MainController : MonoBehaviour
+{
+    private GameObject[] items;
+    private Vector3[] initialPositions;
+    private GetHelper get;
+    public Camera cam;
+    private bool once = false;
+    void Awake()
+    {
+        items = GameObject.FindGameObjectsWithTag("Item");
+        initialPositions = new Vector3[items.Length];
+        for (int i = 0; i < items.Length; i++)
+        {
+            initialPositions[i] = items[i].transform.position;
+        }
+    }
+
+    
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+        if (cam != null)
+        {
+            // Tie this to the camera, and do not keep the local orientation.
+            transform.SetParent(cam.GetComponent<Transform>(), true);
+        }
+        get = GameObject.Find("get").GetComponentInChildren<GetHelper>();
+        M2XSchema res = get.m2x();
+
+        Debug.Log("This is res " + res.ToString());
+
+    }
+
+    void LateUpdate()
+    {
+        RaycastHit hit;
+        Physics.Raycast(cam.transform.position, cam.transform.forward, out hit);
+        Transform go_focus = hit.transform;
+        for (int i = 0; i < items.Length; i++)
+        {
+            GvrAudioSource src = items[i].GetComponentInChildren<GvrAudioSource>();
+            float updown = cam.transform.rotation.x;
+            src.volume = 0.5f - updown;
+            //Debug.Log(0.5 - updown);
+            if (items[i].transform.position.Equals(initialPositions[i]))
+                items[i].transform.position = initialPositions[i];
+        }
+        if (go_focus)
+        {
+            if (!once)
+            {
+                go_focus.position = Vector3.MoveTowards(go_focus.transform.position, cam.transform.position, 2f);
+                GvrAudioSource src = go_focus.GetChild(0).GetComponent<GvrAudioSource>();
+                src.Play();
+                //Debug.Log(src.volume);
+            }
+
+            once = true;
+        }
+        else
+        {
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                GvrAudioSource src = items[i].GetComponentInChildren<GvrAudioSource>();
+                
+                items[i].transform.position = initialPositions[i];
+            }
+            once = false;
+        }
+    }
+}
